31e6189210ba1aedf728c98ca9afd922aff4dc8a
swixx55 <royh-88@hotmail.com>
2018-06-10 22:15:23 +0200

    .

--- src/mypackage/BMIServiceImpl.java
+++ src/mypackage/BMIServiceImpl.java
@@ -3,9 +3,7 @@ package mypackage;
 import javax.jws.WebService;
 import java.math.BigInteger;
 
-/**
- * Created by Roy on 23-6-2017.
- */
+
 
 @WebService( endpointInterface= "mypackage.WSInterface")
 public class BMIServiceImpl implements WSInterface {
